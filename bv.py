dbbd2d7684e52cfc8a13e082ebd363352c6bc904
jun1-per <jun1per@tutamail.com>
2024-05-29 17:34:46 +0300

    Updated npm packages

---
+++ bv.py
@@ -0,0 +1,7 @@
+a1,s2=map(int,input().split(" "))
+r3,d4=map(int,input().split(" "))
+f1,b2=map(int,input().split(" "))
+if((a1==r3==f1) or (a1==s2) or (r3==d4) or (f1==b2)):
+print("yes")
+else:
+print("no")
